2dc704c518e7e830324d30497be05d9a3fa0c288
Stephanie <stephanie.barrantes21@gmail.com>
2017-03-17 12:57:48 -0600

    Prueba de error
    No le hagan caso.

---
+++ components/students/students.controller.js
@@ -0,0 +1,103 @@
+(function(){
+  angular
+    .module('cshApp')
+    .controller('studentController', studentController);
+    function studentController(studentService,ImageService,Upload){ //se inyecta el service userService en el controlador para que se tenga acceso
+      //controlador
+      var studentCtrl = this; //binding del controlador con el html, solo en el controlador
+     studentCtrl.cloudObj = ImageService.getConfiguration();
+
+      function init(){ // función que se llama así misma para indicar que sea lo primero que se ejecute
+        studentCtrl.//actorList = studentService.getStudent(); que lista es la que se pone?
+      }
+      init();
+
+      studentCtrl.preSave = function(){
+        studentCtrl.cloudObj.data.file = document.getElementById("photo").files[0];
+        Upload.upload(studentCtrl.cloudObj)
+          .success(function(data){
+            studentCtrl.save(data.url);
+          });
+      }
+
+      studentCtrl.preEdit = function(){
+        studentCtrl.cloudObj.data.file = document.getElementById("photo").files[0];
+        Upload.upload(studentCtrl.cloudObj)
+          .success(function(data){
+            studentCtrl.confirmarEditarActor(data.url);
+          });
+      }
+
+      studentCtrl.save= function(pimage){
+        var newStudent ={
+          nombre : studentCtrl.nombreActor,
+          premio : studentCtrl.premio,
+          image: pimage
+        }
+
+        studentService.addStudent(newStudent);
+
+        studentCtrl.nombreActor = null;
+        studentCtrl.premio = null;
+        studentCtrl.imagenActor = null;
+
+
+      }
+
+      studentCtrl.borrarActor = function(){
+              // do something on click
+        var listaActor = studentCtrl.actorList;
+        var select = studentCtrl.studenteEliminar.nombre;
+
+        for (var i = 0; i < listaActor.length; i++) {
+          var nombreActor = listaActor[i].nombre;
+          if (nombreActor == select) {
+            listaActor.splice(i);
+            actorService.setLocal(listaActor);
+          }
+        }
+      }
+
+      studentCtrl.editarActor = function(){
+        var listaActor = studentCtrl.actorList;
+        var select = studentCtrl.actorEditar.nombre;
+        
+        for (var i = 0; i < listaActor.length; i++) {
+            var nombreAct = listaActor[i].nombre;
+              if (nombreAct == select) {
+                studentCtrl.nombreActorEdit = listaActor[i].nombre,
+                studentCtrl.premioEdit = listaActor[i].premio;
+                studentCtrl.imagenActor = listaActor[i].image;
+          }
+        }
+      }
+
+    studentCtrl.confirmarEditarActor = function(pImagenEdit){
+        var listaActor = studentCtrl.actorList;
+        var campoNombre = studentCtrl.nombreActorEdit;
+        var campoPremio = studentCtrl.premioEdit;
+        var campoImagen = pImagenEdit;
+        var indiceActor = studentCtrl.actorEditar.$$mdSelectId;
+        var Actor = [];
+
+        for (var i = 0; i < listaActor.length; i++) {
+          var indiceLista = i+1;
+          if (indiceLista == indiceActor) {
+            var newActor ={
+              nombre : campoNombre,
+              premio : campoPremio,
+              image : campoImagen
+            }
+            Actor.push(newActor);
+          }
+          else{
+              Actor.push(listaActor[i]);
+            }
+        }
+        actorService.setLocal(Actor);
+      }
+
+    }
+    
+     //se establece un objeto de angular normal
+})()
---
+++ components/students/students.service.js
@@ -0,0 +1,40 @@
+(function(){
+  angular
+  .module('cshApp')
+  .service('studentService', studentService);
+
+  function studentService(){
+    var student = [];
+    var publicAPI = {
+        addStudent : _addStudent,
+        getStudent : _gettudent,
+        setLocal : localStorageStudent
+    };
+    return publicAPI; // todas las funciones que sean llamadas por ajax deben estar debajo del return, para que ciuando angular corra el script haga el return y devuelva el api , las funciones debajo del return son privadas y se devuelve el api que es el que contiene las funciones
+
+
+    function addStudent(pStudent){
+      student.push(pStudent);
+      console.log(pStudent);
+      localStorageStudent(pStudent);
+    }
+
+    function getStudent(){
+      var listaStored = localStorage.getItem('localStudent');
+      if (listaStored == null ) {
+        student = [];
+      }else {
+        student = JSON.parse(listaStored);
+      }
+      return student;
+    }
+
+    function localStorageStudent(pStudent){
+      localStorage.setItem(['localStudent'], JSON.stringify(pStudent));
+    }
+
+
+
+  }
+
+})();
