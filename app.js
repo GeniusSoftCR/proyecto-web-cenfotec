eb684d78ca8d1abf751ea4990cd72554835d01da
Andres Castillo <aazcast@gmail.com>
2017-03-18 11:21:31 -0600

    app.js changes

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 (function(){
-	angular.module('cshApp', ['ui.router']);
+  angular.module('cshApp', ['ui.router', 'angular-filepicker', 'ngFileUpload']).config(function (filepickerProvider) {
+    filepickerProvider.setKey('ATHNKI5KATbile6a9svfXz');
+  });
 })();
