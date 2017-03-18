dca6c4094e7ea8e4473b935b7dae653825063d4f
Stephanie <stephanie.barrantes21@gmail.com>
2017-03-18 09:48:59 -0600

    other changes

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
-(function(){
+/*(function(){
   angular.module('cshApp', ['appRoutes']);
-})();
+})();*/
 
 //https://material.angularjs.org/latest/Theming/03_configuring_a_theme
+(function(){
+  angular.module('cshApp', ['ui.router','ngFileUpload'])
+  .config(function($mdThemingProvider) {
+  $mdThemingProvider.theme('default')
+    .primaryPalette('purple')
+    .accentPalette('green');
+});//red, pink, purple, deep-purple, indigo, blue, light-blue, cyan, teal, green, light-green, lime, yellow, amber, 
+  //orange, deep-orange, brown, grey, blue-grey
+})();
--- index.route.js
+++ index.route.js
@@ -1,7 +1,43 @@
 (function(){
+  angular
+    .module('cshApp')
+    .config(configuration)
+
+    function configuration($stateProvider, $urlRouterProvider){ //stateProvider
+      
+
+      $stateProvider
+        .state('students',{
+          url: '/students',
+          templateUrl: 'components/students/students.view.html',
+          controller: 'studentController',
+          controllerAs: 'studentCtrl'
+        })
+       
+
+        $urlRouterProvider.otherwise('/students');
+    }
+
+
+
+})();
+
+
+
+
+
+
+
+
+
+
+
+/*
+(function(){
   'use strict';
   angular
-    .module('appRoutes',['ui.router'])
+  .module('cshApp')
+  /*.module('appRoutes',['ui.router'])
     .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
       $stateProvider
         .state('students',{
@@ -10,8 +46,12 @@
           controller: 'studentController',
           controllerAs: 'studentCtrl'
         });
-      $urlRouterProvider.otherwise("/"); 
-      $locationProvider.html5Mode(true);
-      $locationProvider.hashPrefix("");  
+      $urlRouterProvider.otherwise("/students"); 
     });
 });
+
+
+      $locationProvider.html5Mode(true);
+      $locationProvider.hashPrefix(""); */
+
+
