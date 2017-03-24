b9c569e87bafcd70b3096878b111551ed77d18bc
KaguilarA <kaguilara@ucenfotec.ac.cr>
2017-03-24 16:01:20 -0600

    Cambo 1

--- components/logIn/logIn.controller.js
+++ components/logIn/logIn.controller.js
@@ -21,7 +21,7 @@
 			
 			vm.loading = true;
 			
-			var logInRequest = AuthService.logIn;
+			var logInRequest = AuthService.logIn();
 			
 			if(logInRequest){
 				$log.info("Login success: "+ logInRequest);
