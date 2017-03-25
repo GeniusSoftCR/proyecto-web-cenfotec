fb08fad53354a859a795e6409ecb83718ee34e98
hmurillop <hmurillop@gmail.com>
2017-03-24 23:07:58 -0600

    reset

--- components/dashboard/header/header.service.js
+++
@@ -1,8 +0,0 @@
-(function(){
-  /* Servicio de Request de proyecto de Cenfotec Software House */
-  'use strict';
-  angular.module('cshApp').service('headerService', headerService);
-    function headerService (localStorageService, $http) {
-     
-    }
-})();
