a1e22d8e0fa5e37c4727931b5b456eadd97129bd
Esteban Fonseca Blanco <emfobla@gmail.com>
2017-04-01 19:31:55 -0600

    update

--- components/students/studentRequests.controller.js
+++ components/students/studentRequests.controller.js
@@ -9,8 +9,11 @@
       var vm = this;
       //inicia cargando la lista de solicitudes
       vm.requestsList = studentRequestsService.getRequests();
+      vm.btnYes=true;
+      vm.btnNo=true;
       //oculta secciones del view
-      vm.rejection=false;
+      vm.rejection=false;//contiene la justificación
+      vm.confirm=false;//botón para confirmar el rechazo
 
       //recargar la lista de solicitudes
       vm.fetchRequestsList= function(){
@@ -21,6 +24,13 @@
       vm.viewRequest= function(request){
         vm.req=request;
         // setTimeout(function(){$('#myModal').modal('hide')},3000);
+      }
+      //ocultar confirmación
+      vm.hideConfirm= function(request){
+        vm.rejection=false;
+        vm.confirm=false;
+        vm.btnYes=true;
+        vm.btnNo=true;
       }
       vm.approveRequest= function(request){
         //1)1er param:solicitud actual, 2do param: estado(rechazado=2)
@@ -36,6 +46,7 @@
         studentRequestsService.changeRequestState(request,3);
         //2)oculta la sección de la justificación
         vm.rejection=false;
+        vm.confirm=false;
         //3)actualizar la lista de solicitudes
         vm.fetchRequestsList();
         /*4)Back End:enviar notificación por correo*/
