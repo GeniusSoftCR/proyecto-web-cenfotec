ae03f43f20f83f434135dc6b29741f2c59907fe0
Esteban Fonseca Blanco <emfobla@gmail.com>
2017-04-05 23:51:53 -0600

    update

--- components/students/studentRequests.controller.js
+++ components/students/studentRequests.controller.js
@@ -37,22 +37,32 @@
         studentRequestsService.changeRequestState(request,2);
         //3)actualizar la lista de solicitudes
         vm.fetchRequestsList();
-        /*4)Back End:enviar notificación por correo*/
-      }
-      vm.rejectRequest= function(request){
-        if(vm.req.justification!=""){
-        vm.validation=false;
-        //1)1er param:solicitud actual, 2do param: estado(rechazado=3)
-        studentRequestsService.changeRequestState(request,3);
-        //2)oculta la sección de la justificación
-        vm.rejection=false;
-        vm.confirm=false;
-        //3)actualizar la lista de solicitudes
-        vm.fetchRequestsList();
         //cerrar el modal
         $('#studentReq-Modal').modal('hide');
         /*4)Back End:enviar notificación por correo*/
-        }else{vm.validation=true;}
+      }
+      vm.rejectRequest= function(request){
+        if(vm.req.justification!=null){
+          vm.validate=false;
+          //1)1er param:solicitud actual, 2do param: estado(rechazado=3)
+          studentRequestsService.changeRequestState(request,3);
+          //2)oculta la sección de la justificación
+          vm.rejection=false;
+          vm.confirm=false;
+          //3)actualizar la lista de solicitudes
+          vm.fetchRequestsList();
+          //cerrar el modal
+          $('#studentReq-Modal').modal('hide');
+          vm.rejection=false;
+          vm.confirm=false;
+          vm.btnYes=true;
+          vm.btnNo=true;
+          /*4)Back End:enviar notificación por correo*/
+        } else { 
+          vm.validate=true;
+          $('#justification').closest('.form-group').addClass('has-error');
+          vm.req.justification=null;
+        }
       }
 
     }
