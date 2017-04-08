82ed903ec263a19c29223d67d87b117d7912dc12
Stephanie <stephanie.barrantes21@gmail.com>
2017-04-08 02:56:29 -0600

    Cambios arreglados

--- components/projects/project/projectAnotations/projectanotations.service.js
+++ components/projects/project/projectAnotations/projectanotations.service.js
@@ -1,45 +1,3 @@
-<<<<<<< HEAD
-(function(){
-  /* Servicio de Anotaciones de proyecto de Cenfotec Software House */
-  'use strict';
-  angular.module('cshApp').service('projectAnotationsService', projectAnotations);
-    function projectAnotations () {
-      var anotations = [];
-      //   {
-      //     id : 1,
-      //     projectId : 1,
-      //     name : 'La primera anotación',
-      //     description : 'Esta es la descripción general de la anotación que estamos realizando, algo muy interesante',
-      //     iduserCreate: 1
-      //   }
-      // ];
-      var publicApi = {
-        addAnotation : _addAnotation,
-        getAnotations : _getAnotations,
-        deleteAnotation : _deleteAnotation,
-        putAnotation : _putAnotation
-      };
-      return publicApi;
-
-      function _addAnotation (pAnotation) {
-        anotations.push(pAnotation);
-      }
-
-      function _getAnotations () {
-        return anotations;
-      }
-
-      function _deleteAnotation (pAnotation) {
-        anotations.splice(pAnotation, 1);
-      }
-
-      function _putAnotation (pAnotation, pFinalAnotation) {
-        _deleteAnotation(pAnotation);
-        _addAnotation(pFinalAnotation);
-      }
-
-    }
-=======
 (function(){
   /* Servicio de Anotaciones de proyecto de Cenfotec Software House */
   'use strict';
@@ -80,5 +38,4 @@
       }
 
     }
->>>>>>> dev-kaguilara
 })();
--- components/projects/projectRequest2/projectRequests.service.js
+++ components/projects/projectRequest2/projectRequests.service.js
@@ -1,50 +1,3 @@
-<<<<<<< HEAD:components/projects/projectRequest2/projectRequests.service.js
-(function(){
-  angular
-  .module('cshApp')
-  .service('projectRequestsService', projectRequestsService);
-
-  projectRequestsService.$inject = ['$http', 'localStorageService'];
-
-  function projectRequestsService($http,localStorageService){
-    
-    var publicAPI = {
-      getRequests : _getProjects,
-      changeRequestState : _changeProjectsState
-    };
-    return publicAPI;
-
-    //trae la lista de proyectos
-    function _getProjects(){
-      var storedList = localStorageService.get('localProjectsList');
-      if(storedList == null){
-        projectsList = [];
-      }else{
-        projectsList = storedList;
-      }
-      return projectsList;
-    }
-
-    //cambia el estado a aprobado o rechazado según el parámetro
-    function _changeProjectsState(request,newState){
-      request.state_key=newState;
-      var index = null;
-      angular.forEach(projectsList, function(student, position) {
-        if (student.mail === request.mail) {
-          index = position;
-        }
-      });
-      projectsList[index]=request;
-      localStorageStudentsList(projectsList);
-    }
-    //inserta los nuevos registros al localStorage
-    function localStorageStudentsList(jlist){
-      localStorageService.set('localProjectsList', jlist);
-    }
-
-  }
-
-=======
 (function(){
   angular
   .module('cshApp')
@@ -52,7 +5,7 @@
 
   projectRequestsService.$inject = ['$http','cshReqServiceFn','localStorageService'];
 
-    
+  function projectRequestsService($http,localStorageService){
     var publicAPI = {
       getRequests : _getProjects,
       changeProjectsState : _changeProjectsStates
@@ -88,6 +41,4 @@
     }
 
   }
-
->>>>>>> dev-kaguilara:components/projects/project/projectRequest2/project.service.js
 })();
