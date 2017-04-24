763c87206bcd02a20cdccfc3d5fc12a321d0a17e
Stephanie <stephanie.barrantes21@gmail.com>
2017-04-24 00:37:29 -0600

    formsUsers

--- public/components/administratorSteph/registerUser/userAp.controller.js
+++ public/components/administratorSteph/registerUser/userAp.controller.js
@@ -25,9 +25,37 @@
     });
 });
 
+        //En el input de Avatar muestra al lado de escoger, la imagen que se ha seleccionad
+          $(function() {
+
+            // We can attach the `fileselect` event to all file inputs on the page
+            $(document).on('change', ':file', function() {
+              var input = $(this),
+                  numFiles = input.get(0).files ? input.get(0).files.length : 1,
+                  label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+              input.trigger('fileselect', [numFiles, label]);
+            });
+
+            // We can watch for our custom `fileselect` event like this
+            $(document).ready( function() {
+                $(':file').on('fileselect', function(event, numFiles, label) {
+
+                    var input = $(this).parents('.input-group').find(':text'),
+                        log = numFiles > 1 ? numFiles + ' files selected' : label;
+
+                    if( input.length ) {
+                        input.val(log);
+                    } else {
+                        if( log ) alert(log);
+                    }
+
+                });
+            });
+          });
+
       vm.userList = [];
       
-      vm.userList = userService.getUsers();
+      vm.userList = userService.getUser();
 
       vm.rejection=false;
       vm.edit.modal=false;
