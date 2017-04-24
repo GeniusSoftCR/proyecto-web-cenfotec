ff0e695075e8acefd98dcc1bc7361d1927abb6c3
Esteban Fonseca Blanco <emfobla@gmail.com>
2017-04-24 11:43:57 -0600

    update

--- public/components/students/studentRequests.controller.js
+++ public/components/students/studentRequests.controller.js
@@ -16,7 +16,7 @@
       vm.rejection=false; //oculta bloque de la jsutificación
       vm.confirm=false;   //oculta botón de confirmar
 
-      //Recargar la lista de solicitudes
+      //RECARGAR LISTA DE SOLICITUDES
       vm.reloadPage = function () {
         setTimeout(function(){$window.location.reload()},1)
       }
@@ -28,7 +28,7 @@
       }
       vm.fetchRequestsList();
 
-      //Mostrar el detalle de la solicitud
+      //MOSTRAR DETALLE DE LA SOLICITUD
       vm.viewRequest= function(request){
         vm.req=request;     //binding de la solicitud seleccionada
         //en el modal:
@@ -39,21 +39,20 @@
         $('#justification').closest('.form-group').removeClass('has-error');
         vm.req.justification=null;
         vm.finalStep=false;
-        // setTimeout(function(){$('#rejectRetro').modal('hide')},3000);
-        //$('#retro').show('slow');
       }
 
-      //Aprobar una solicitud
+      //APROBAR SOLICITUD
       vm.approveRequest= function(request){
         //1)1er param:solicitud actual, 2do param: estado(aprobado=2)
         userService.changeRequestState(request,"eligible").then(function(res){
           console.log("Estudiante aprobado" + res.data);
         });
+        // setTimeout(function(){$('#rejectRetro').modal('hide')},3000);
+        //$('#retro').show('slow');
         //3)actualizar la lista de solicitudes
         vm.fetchRequestsList();
         //cerrar el modal
         $('#studentReq-Modal').modal('hide');
-        /*4)Back End:enviar notificación por correo*/
       }
 
       vm.switch=function(){
@@ -63,39 +62,33 @@
 
       vm.confirmation=function(x,y){
         userService.changeRequestState(x,y);
-          vm.fetchRequestsList();
-          vm.reloadPage();
+        //3)actualizar la lista de solicitudes
+        vm.fetchRequestsList();
+        vm.reloadPage();
       }
 
-      //Rechazar una solicitud
+      //RECHAZAR SOLICITUD
       vm.rejectRequest= function(request){
         vm.finalStep=false;
-        //si el input de la justificación no está vacío
+        //si la justificación no está vacía
         if(vm.req.justification!=null){
-          vm.validate=false;  //oculta mensaje "justificación requerida"
+          //vm.validate=false;  //oculta mensaje "justificación requerida"
           $('#justification').closest('.form-group').removeClass('has-error');
-          //1)1er param:solicitud actual, 2do param: estado(rechazado=3)
-          //userService.changeRequestState(request,"rejected");
-          //2)oculta la sección de la justificación
-          vm.rejection=false;
-          vm.confirm=false;
-          //3)actualizar la lista de solicitudes
-          //vm.confirmation(request,"rejected");
+          //1)seteo de parámetros. 1er param:solicitud actual, 2do param: estado(rechazado=3)
           vm.param1=request;
           vm.param2="rejected";
-          //cerrar el modal
+          //2)mantenimientos
           vm.rejection=false;
           vm.confirm=false;
           vm.btnYes=true;
           vm.btnNo=true;
-          // setTimeout(function(){$('#retro-Modal').modal('hide')},3000);
-          /*4)Back End:enviar notificación por correo*/
           vm.finalStep=true;
         } else { 
-          vm.validate=true;
+          //vm.validate=true;
           $('#justification').closest('.form-group').addClass('has-error');
           vm.req.justification=null;
         }
       }
     }
+
 })();
