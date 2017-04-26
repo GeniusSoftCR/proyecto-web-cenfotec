e37ccf5c73c658656729ca74d7263689e5fc8e68
Esteban Fonseca Blanco <emfobla@gmail.com>
2017-04-26 15:11:02 -0600

    update

--- public/components/projects/viewProject/viewProjects.controller.js
+++ public/components/projects/viewProject/viewProjects.controller.js
@@ -5,8 +5,11 @@
     viewProjectsController.$inject= ['projectService','SessionService'];
 
     function viewProjectsController(projectService,SessionService){
-      console.log(SessionService.session.role);
       var vm = this;
+      console.log(SessionService.session.role);
+      if(SessionService.session.role=="student"){
+        vm.test=true;
+      }
       //inicia cargando la lista de estados de proyecto
       projectService.getProjects().then(function(res){
         vm.projects =  res.data;
