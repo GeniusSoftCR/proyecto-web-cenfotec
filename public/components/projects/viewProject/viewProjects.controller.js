cfec12d1728b3d65fb0e54a6cb80a2bf35be163d
Esteban Fonseca Blanco <emfobla@gmail.com>
2017-04-26 16:29:22 -0600

    update

--- public/components/projects/viewProject/viewProjects.controller.js
+++ public/components/projects/viewProject/viewProjects.controller.js
@@ -7,9 +7,18 @@
     function viewProjectsController(projectService,SessionService){
       var vm = this;
       console.log(SessionService.session.role);
-      if(SessionService.session.role=="student"){
-        vm.test=true;
-      }
+      // if(SessionService.session.role=="student"){
+      //   vm.test=true;
+      // }
+      switch(SessionService.session.role){
+        case "admin":
+        case "assistant":
+          vm.test=true;
+          break;
+        default:
+          vm.test=false;
+          break;  
+      };
       //inicia cargando la lista de estados de proyecto
       projectService.getProjects().then(function(res){
         vm.projects =  res.data;
