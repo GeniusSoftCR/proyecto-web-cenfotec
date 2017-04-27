4267b25342f485c989708fbc4f56b454694e00ee
Esteban Fonseca Blanco <emfobla@gmail.com>
2017-04-27 15:31:52 -0600

    update

--- public/components/projects/viewProject/viewProjects.controller.js
+++ public/components/projects/viewProject/viewProjects.controller.js
@@ -2,37 +2,41 @@
   angular
     .module('cshApp')
     .controller('viewProjectsController', viewProjectsController);
-    viewProjectsController.$inject= ['projectService','SessionService'];
+    viewProjectsController.$inject= ['$q','projectService','SessionService'];
 
-    function viewProjectsController(projectService,SessionService){
+    function viewProjectsController($q,projectService,SessionService){
+
       var vm = this;
+      var request=null;
       vm.flag=false;  //bandera de veirificacion
       vm.empty=false; //mensaje de lista vacía
       vm.asignedProjects = [];  //filtro de proyectos
+      vm.teacher=SessionService.session.idNum;
+
+      projectService.getProjects({}).then(function(res){
+        $q.when(res).then(function () {
+          console.log("Funcione!!!");
+          vm.projects=res.data;
+          console.log(vm.projects);
+        })
+      });
+
 
       //TRAE LA LSITA DE PROYECTOS
-      projectService.getProjects({}).then(function(res){
-        vm.projects =  res.data;
-      });
       //FILTRA el select del view y FILTRA la lista de proyectos
       switch(SessionService.session.role){
         case "admin":
+          console.log("Admin"+ vm.projects);
         case "assistant":
           vm.test=true;
           break;
         case "professor":
-          vm.teacher=SessionService.session.idNum;
-          //buscar los proyectos a los que el profesor actual ha sido asignado
-          for (i = 0; i < vm.projects.length; i++){
-            if((vm.projects[i].professor === vm.teacher)||(vm.projects[i].assistant === vm.teacher)){
-              //filtra por proyectos "en proceso" y/o "finalizados"
-              if((vm.projects[i].state === "inProcess")||(vm.projects[i].state === "ended")){
-                vm.asignedProjects.push(vm.projects[i]);
-              }
-            }
-          }
-          vm.projects=vm.asignedProjects;
+        
+        angular.forEach(vm.projects, function(value, key) {
+          console.log(key + ': ' + value);
+        });
       };
+
       //verificar si la lista(según el estado) está vacía
       vm.verify= function(){
         vm.empty=false;
