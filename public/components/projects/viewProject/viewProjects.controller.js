1b61e52fb209b4528856caa01f50c6e80c7fb1c1
Esteban Fonseca Blanco <emfobla@gmail.com>
2017-04-27 13:16:11 -0600

    update

--- public/components/projects/viewProject/viewProjects.controller.js
+++ public/components/projects/viewProject/viewProjects.controller.js
@@ -6,37 +6,30 @@
 
     function viewProjectsController(projectService,SessionService){
       var vm = this;
-      vm.flag=false;
-      vm.message=false;
+      vm.flag=false;//si la lista está vacía
+      vm.empty=false;
 
-      //el switch filtra el select del view y realiza peticiones de acuerdo al rol de usuario
+      //trae la lista de proyectos
+      projectService.getProjects({}).then(function(res){
+        vm.projects =  res.data;
+      });
+      //el switch filtra el select del view
       switch(SessionService.session.role){
         case "admin":
         case "assistant":
           vm.test=true;
-          vm.param ={};
-          projectService.getProjects(vm.param).then(function(res){
-            vm.projects =  res.data;
-          });
-          break;
-        default:
-          vm.test=false;
-          vm.param ={"professor":SessionService.session.idNum};
-          projectService.getProjects(vm.param).then(function(res){
-            vm.projects =  res.data;
-          });
-          break;  
+          break; 
       };
-
+      //verificar si la lista(según el estado) está vacía
       vm.verify= function(){
-        for (var i = vm.projects.length - 1; i >= 0; i--) {
+        for (i = 0; i < vm.projects.length; i++){
           if(vm.projects[i].state==vm.search){
             vm.flag=true;
             break;
-          }
+          }else{vm.flag=false;}
         }
         if(vm.flag==false){
-          vm.message=true;//muestra el mensaje de lista vacía
+          vm.empty=true;//muestra el mensaje de lista vacía
         }
       }
 
