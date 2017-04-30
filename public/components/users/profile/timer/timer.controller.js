33f1e985b07db18228ec3bcf345bc2e8272cba69
Hector <hmurillop@gmail.com>
2017-04-30 02:52:18 -0600

    timer routing

---
+++ public/components/users/profile/timer/timer.controller.js
@@ -0,0 +1,18 @@
+(function () {
+	'use strict';
+
+	angular.module('cshApp')
+	.controller('timerController', timerController);
+
+	timerController.$inject = [];
+
+ 	function timerController (){
+	
+ 		//vm = view model
+		var vm = this;
+		//////////////////////////////////////////
+
+		//////////////////////////////////////////
+
+	};
+})();
