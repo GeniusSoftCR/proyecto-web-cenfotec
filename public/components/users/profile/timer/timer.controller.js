a873e6ac7635fabc991f05c777d51115cad9d366
Hector <hmurillop@gmail.com>
2017-04-30 05:32:17 -0600

    timer working
    Pendiente salvar

--- public/components/users/profile/timer/timer.controller.js
+++ public/components/users/profile/timer/timer.controller.js
@@ -4,15 +4,62 @@
 	angular.module('cshApp')
 	.controller('timerController', timerController);
 
-	timerController.$inject = [];
+	timerController.$inject = ['$q','$interval','projectService','AuthService'];
 
- 	function timerController (){
+ 	function timerController ($q,$interval,projectService,AuthService){
 	
  		//vm = view model
 		var vm = this;
 		//////////////////////////////////////////
-
+		vm.startCount = _startCount;
+		vm.stopCount = _stopCount;
+		vm.setProject = _setProject;
 		//////////////////////////////////////////
+		vm.user = AuthService.getAuthUser();
+		vm.time = {};
+		vm.project = undefined;
+		vm.time.hour = '0';
+		vm.time.min = '0';
+		vm.time.sec='0';
+		vm.showCero = true;
+		vm.counting = false;
+		
+		/////////////////////////////////////////
+		projectService.getProjects({students:{_id:vm.user._id}}).then(function (res) {
+			$q.when(res).then(function () {
+				console.log(res)
+				vm.projects = res.data;
+			});
+		});
+		function _startCount() {
+			vm.counting = true;
+			$interval(function () {
+				if (vm.time.sec >= '9') {vm.showCero = false;}else{vm.showCero = true;}
+				vm.time.sec++
 
+				if (vm.time.sec == '60') {
+					vm.time.sec = '0';
+					vm.time.min++;
+				};				
+
+				if (vm.time.min == '60') {
+					vm.time.hour++;
+					vm.time.min = '00';
+				};
+
+			},1000)
+		};
+		function _stopCount() {
+			vm.counting = false;
+		}
+		function _setProject(id) {
+			projectService.getProjects({_id:id}).then(function (res) {
+				$q.when(res).then(function () {
+					vm.project = res.data[0];
+				});
+			});
+		};
 	};
 })();
+
+
