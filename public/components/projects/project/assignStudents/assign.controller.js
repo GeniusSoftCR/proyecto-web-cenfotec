8bd3357786624e6d234ea1c84d87e8bad9181831
Kevin Aguilar Alvarado <kaguilara@ucenfotec.ac.cr>
2017-05-01 12:12:47 -0600

    cambios

--- public/components/projects/project/assignStudents/assign.controller.js
+++ public/components/projects/project/assignStudents/assign.controller.js
@@ -7,16 +7,32 @@
 
   assignStudents.$inject = ['$q','$stateParams','projectService', 'userService'];
 
-  function assignStudents ($q, $stateParams, projectService, userServ) {
+  function assignStudents ($q, $stateParams, projectService, userService) {
     var vm = this;
     vm.project = {};
+    vm.students = {};
     vm.add = false;
     vm.del = false;
 
-    //proyecto actual
-    projectService.getProjects({_id:$stateParams.id}).then(function(res){
-      vm.project = res.data[0];
+    //traer lista de esudiantes
+    /*vm.students = userService.getUsers({role:"student"}).then(function (res) {
+      vm.students=res.data;
+      console.log(vm.students.idNum);
+    });*/
 
+    //traer lista de esudiantes
+    userService.getUsers({"role":"student"}).then(function (res) {
+      vm.students = res.data;
+      init();
     });
+
+      /*projectService.getProjects({_id:$stateParams.id}).then(function (res) {
+        vm.project=res.data[0];
+        init();
+      });*/
+
+      function init(){
+        console.log(vm.students);
+      }
   };
 })();
