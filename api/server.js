5fad5c53848040924c1658b26cfd3cf2ab27f393
Hector <hmurillop@gmail.com>
2017-05-01 01:30:43 -0600

    socket io

--- api/server.js
+++ api/server.js
@@ -1,39 +1,49 @@
 	//Definimos las dependencias
 var express = require('express'),
+	app = express(),
 	path = require('path'),
 	bodyParser = require('body-parser'),
 	morgan = require('morgan'),
 	mongoose = require('mongoose'),
-	//Variables del back de la aplicacion
-	app = express(),
-	io = require('socket.io'),
+	//Variables del back de la aplicacion	
+	db = mongoose.connection;
+	dburl = 'mongodb://admin:proyectoweb1@ds155130.mlab.com:55130/csh',
 	port = 3000,
-	dburl = 'mongodb://admin:proyectoweb1@ds155130.mlab.com:55130/csh';
+	server = app.listen(port,_server()),
+	socket = require('socket.io').listen(server),
+	io = socket.sockets;
 
+	io.on('connection', function (socket) {
+	    console.log('client connect');
+
+	    socket.emit('message', { msg: 'Hello mr. client' });
+
+	    socket.on('echo', function (data) {
+	       console.log(data)
+	    });
+	});
+
+
+
+
+// io.on('connection', function (socket) {
+//   socket.emit('news', { hello: 'world' });
+//   socket.on('my other event', function (data) {
+//     console.log(data);
+//   });
+// });	
 
 //Se define la conexion con Mongoose
 mongoose.connect(dburl);
 
-var server = app.listen(port,_server());
-var db = mongoose.connection;
-
 db.on('error', console.error.bind(console, 'connection error:'));
-
 db.once('open', function() {
   // Se conecto correctamente!
   console.log('mongo database conected');
 });
 
-//We define files where we are gonna generated main routes for the app
-var index = require('./index'),
-	users = require('./users/user.api'),
-	projects = require('./projects/project.api'),
-	config = require('./config/config.api.js');
-
-	
 // Set static Folder
-// app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, 'public')));
 //Body Parser MW
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -47,15 +57,26 @@ app.use(function (req, res, next) {
   next();
 });
 
+app.use(function(req,res,next){
+    req.io = io;
+    next();
+});
+
+//We define files where we are gonna generated main routes for the app
+var index = require('./index'),
+	users = require('./users/user.api'),
+	projects = require('./projects/project.api'),
+	config = require('./config/config.api.js');
+
 //Define Express Routes
 app.use('/api', users);
 app.use('/api', projects);
 app.use('/api', config);
 app.use('/', index);
 
+module.exports = app;
 
 ///////////////////// + Private + ////////////////////////
-
 function _server() {
 	console.log('server started on port ' + port);
 }
--- public/components/users/profile/profile.controller.js
+++ public/components/users/profile/profile.controller.js
@@ -7,10 +7,14 @@
 	profileController.$inject = ['$stateParams','AuthService'];
 
 	function profileController ($stateParams,AuthService) {
+
 		var vm = this;
 		vm.user = {};
 		vm.user = AuthService.getAuthUser();
-		console.log(vm.user)
+
+
 		//vm.user.username = $stateParams.username;		
+
+
 	};
 })();
