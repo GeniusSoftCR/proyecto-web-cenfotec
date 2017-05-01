5180c453e9504a7b2abee76402bf52fdbedadbc2
Esteban Fonseca Blanco <emfobla@gmail.com>
2017-05-01 05:35:11 -0600

    update

--- public/components/projects/project/assignTeachers/assign.controller.js
+++ public/components/projects/project/assignTeachers/assign.controller.js
@@ -1,8 +1,7 @@
 (function(){
   'use strict';
   angular.module('cshApp')
-    .controller('assignTeachersController', assignTeachersController)
-    .filter('startFrom'/*, pagination*/);
+    .controller('assignTeachersController', assignTeachersController);
 
     assignTeachersController.$inject = ['$q','$stateParams','projectService', 'userService'];
 
@@ -94,47 +93,6 @@
         }
       }
 
-
-      //var mainProject = watchProjectService.getProjectbyId(vm.projectId);
-      //trae lista de profesores
-      //var teachers = userProfessorService.getProfessors();
-      //disponibilidad para proyectos
-
-      // vm.currentPage = 0;
-      // vm.pageSize = 1;
-      // vm.numberOfPages=function(){
-      //     return Math.ceil(vm.teachers.length/vm.pageSize);                
-      // }
-      
-      // vm.assignTeacher = function () {
-      //   var teacherSelected = vm.assign.teacherChecked;
-      //   var project = watchProjectService.getProjectbyId(vm.projectId);
-      //   if (project.assitant == null) {
-      //       project.assitant = [];
-      //   }
-      //   project.assitant.push(teacherSelected);
-      //   var updateProjectRequest ={
-      //     name: project.name,
-      //     id : vm.projectId,
-      //     state_key : project.state_key,
-      //     clientId: project.clientId,
-      //     professor: project.professor,
-      //     assitant: project.assitant,
-      //     executiveSummary : project.executiveSummary,
-      //     objective: project.objective,
-      //     images:project.images,
-      //     funds : project.fundsToMakeProject,
-      //     students: project.students,
-      //     files :project.files
-      //   }
-      //   cshReqService.putProject(vm.projectId, updateProjectRequest);
-      // }
     }
 
-    // function pagination () {
-    //   return function(input, start) {
-    //       start = +start; //parse to int
-    //       return input.slice(start);
-    //   }
-    // }
 })();
