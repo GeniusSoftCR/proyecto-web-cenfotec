7cdbf78bc4407213b290eab66e16dd7aa2ce70d2
Hector <hmurillop@gmail.com>
2017-05-02 13:17:10 -0600

    conflict fix

--- public/components/users/profile/profile.controller.js
+++ public/components/users/profile/profile.controller.js
@@ -7,14 +7,9 @@
 	profileController.$inject = ['$stateParams','AuthService'];
 
 	function profileController ($stateParams,AuthService) {
-
 		var vm = this;
 		vm.user = {};
 		vm.user = AuthService.getAuthUser();
-
-
 		//vm.user.username = $stateParams.username;		
-
-
 	};
 })();
