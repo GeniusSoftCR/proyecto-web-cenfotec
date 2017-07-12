ee0344333d9cf4689e28a8db57ca5ea44baa697f
KaguilarA <kaguilara@ucenfotec.ac.cr>
2017-07-11 21:45:42 -0600

    fix error

--- api/server.js
+++ api/server.js
@@ -18,32 +18,27 @@ io = require('socket.io').listen(server);
 //Se define la conexion con Mongoose
 mongoose.connect(dburl);
 
-//Se revisa el estado de coneccion de la BD
-var db = mongoose.connection;
-
+//Connection events
 db.on('error', console.error.bind(console, 'connection error:'));
-
 db.once('open', function() {
   // Se conecto correctamente!
   console.log('mongo database conected');
 });
+ 
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { msg: 'Hello client - from : server'});
+  socket.on('echo', function (data) {
+    console.log(data);
+  });
+});
 
-//We define files where we are gonna generated main routes for the app
-var index = require('./index'),
-	users = require('./users/user.api'),
-	projects = require('./projects/project.api'),
-	config = require('./config/config.api');
-	//email = require('./email/email.api');
-
-	
 // Set static Folder
-// app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, 'public')));
 //Body Parser MW
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(morgan('dev'));
-
+//Cross
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -65,14 +60,12 @@ var index = require('./index'),
     projects = require('./projects/project.api'),
     config = require('./config/config.api.js');
 //
-
 //Define Express Routes
 app.use('/api', users);
 app.use('/api', projects);
 app.use('/api', config);
 //app.use('/api', email);
 app.use('/', index);
-
 ///////
 //////
 module.exports = app;
@@ -80,9 +73,5 @@ module.exports = app;
 //
 ///////////////////// + Private + ////////////////////////
 function _server() {
-	console.log('server started on port ' + port);
-}
-
-app.listen(port, function(){
   console.log('server started on port ' + port);
-});
+}
